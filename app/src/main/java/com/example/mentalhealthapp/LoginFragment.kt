382ca3c9762af6be9291f7ed36e0a21b265d070d
Karan Markandey <karan.iitg54@gmail.com>
2023-07-08 02:28:01 +0530

    Email and password login signup done

--- app/src/main/java/com/example/mentalhealthapp/LoginFragment.kt
+++ app/src/main/java/com/example/mentalhealthapp/LoginFragment.kt
@@ -5,55 +5,90 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.example.mentalhealthapp.databinding.FragmentLoginBinding
+import com.example.mentalhealthapp.viewmodels.LoginSignUpVM
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class LoginFragment : Fragment(R.layout.fragment_login) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding : FragmentLoginBinding
+    private lateinit var viewModel: LoginSignUpVM
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+         super.onCreateView(inflater, container, savedInstanceState)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login,container,false)
+        viewModel = ViewModelProvider(this)[LoginSignUpVM::class.java]
+
+        binding.btnLogin.setOnClickListener {
+
+            if(checkEntries()){
+                loginUser()
+                //navigateToHome()
+            }
+
+        }
+
+        binding.tvSigunUp.setOnClickListener(){
+            navigateToSignUpFragment()
+        }
+
+        binding.cvLoginWithGoogle.setOnClickListener {
+            viewModel.logoutUser()
+        }
+
+
+        return binding.root
+
+
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun loginUser(){
+        val email = binding.etLoginEmail.text.toString()
+        val password = binding.etLoginPassword.text.toString()
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = viewModel.logInWithEmailAndPassword(email, password)
+            if(result.first){
+                withContext(Dispatchers.Main){
+                    Toast.makeText(requireContext(),"Logged In...",Toast.LENGTH_SHORT).show()
+                    navigateToHome()
+                }
+            }else{
+                withContext(Dispatchers.Main){
+                    Toast.makeText(requireContext(),result.second,Toast.LENGTH_SHORT).show()
                 }
             }
+
+        }
+
+    }
+
+    private fun navigateToSignUpFragment() {
+        binding.root.findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+    }
+
+    private fun navigateToHome() {
+        binding.root.findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+    }
+
+    private fun checkEntries():Boolean{
+        val email = binding.etLoginEmail.text.toString()
+        val password = binding.etLoginPassword.text.toString()
+
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            return true
+        }
+        Toast.makeText(requireContext(),"Enter credentials...",Toast.LENGTH_SHORT).show()
+        return false
     }
 }
--- app/src/main/java/com/example/mentalhealthapp/SignUpFragment.kt
+++ app/src/main/java/com/example/mentalhealthapp/SignUpFragment.kt
@@ -5,55 +5,85 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.findFragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
+import com.example.mentalhealthapp.databinding.FragmentSignUpBinding
+import com.example.mentalhealthapp.viewmodels.LoginSignUpVM
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SignUpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+    private lateinit var  binding :FragmentSignUpBinding
+    private  lateinit var  viewModel :LoginSignUpVM
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+         super.onCreateView(inflater, container, savedInstanceState)
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_sign_up,container,false)
+        viewModel = ViewModelProvider(this)[LoginSignUpVM::class.java]
+
+        binding.btnSignUp.setOnClickListener {
+            if(checkEntries()){
+                signUpUser()
+            }
+
+
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUpFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignUpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun signUpUser() {
+        val email = binding.etSignUpEmail.text.toString()
+        val password = binding.etSignUpPassword.text.toString()
+       CoroutineScope(Dispatchers.IO).launch {
+           val result = viewModel.signUpWithEmailAndPassword(email, password)
+           if(result.first){
+               withContext(Dispatchers.Main){
+                   Toast.makeText(requireContext(),"Sign Up successful...",Toast.LENGTH_SHORT).show()
+                   navigateToHome()
+               }
+           }else{
+               withContext(Dispatchers.Main){
+                   Toast.makeText(requireContext(),result.second,Toast.LENGTH_SHORT).show()
+               }
+           }
+
+       }
+    }
+
+    private fun navigateToHome (){
+        binding.root.findNavController().navigate(R.id.action_signUpFragment_to_homeFragment)
+    }
+
+    private fun checkEntries():Boolean{
+        val email = binding.etSignUpEmail.text.toString()
+        val password = binding.etSignUpPassword.text.toString()
+        val passwordConfirmed = binding.etConfirmedPassword.text.toString()
+
+        if(email.isNotEmpty() && password.isNotEmpty() && passwordConfirmed.isNotEmpty()){
+            if(password != passwordConfirmed){
+                Toast.makeText(requireContext(),"Enter Credentials...",Toast.LENGTH_SHORT).show()
+                return false
             }
+            else{
+                return true
+
+            }
+        }
+        Toast.makeText(requireContext(),"Enter Credentials...",Toast.LENGTH_SHORT).show()
+        return false
+
     }
 }
