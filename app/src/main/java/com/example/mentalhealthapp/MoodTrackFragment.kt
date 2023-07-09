1615f03d973f5d5931c18d4dad1533c12e1ecfa8
Karan Markandey <karan.iitg54@gmail.com>
2023-07-10 03:06:36 +0530

    Mood calender Added

--- app/src/main/java/com/example/mentalhealthapp/MoodTrackFragment.kt
+++ app/src/main/java/com/example/mentalhealthapp/MoodTrackFragment.kt
@@ -5,8 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.mentalhealthapp.adapter.MoodTrackerAdapter
+import com.example.mentalhealthapp.databinding.FragmentHomeBinding
+import com.example.mentalhealthapp.databinding.FragmentMoodTrackBinding
 
 
 class MoodTrackFragment : Fragment(R.layout.fragment_mood_track) {
 
+    private lateinit var  binding: FragmentMoodTrackBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater , R.layout.fragment_mood_track,container,false)
+
+
+
+        val myAdapter= MoodTrackerAdapter(requireContext())
+
+        binding.gridView.adapter = myAdapter
+
+        return  binding.root
+
+
+    }
 }
